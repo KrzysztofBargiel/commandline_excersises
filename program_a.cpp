ce41803735c24ab0caa590839aa96afe68f05546
KrzysztofBargiel <zychu02@gmail.com>
2014-04-30 15:35:31 +0200

    Done

--- program_a.cpp
+++ program_a.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
 int main(int argc, char * argv[])
 {
     // a. wypisze wszystkie otrzymane argumenty w ososbnych liniach
-    /// YOUR CODE HERE ///
-
-    /// YOUR CODE HERE ///
+    vector<int> vec;
+    
+    
+    if (argc > 1)
+    {
+        ifstream input;
+        input.open(argv[1]);
+        while (input.good())
+        {
+            int num;
+            input >> num;
+            vec.push_back(num);
+        }
+        input.close();
+    }
+    
+    for (auto it = vec.begin(); it != vec.end(); ++it)
+    {
+        cout << *it << endl;
+    }
+    
+    
     return 0;
 }
 
